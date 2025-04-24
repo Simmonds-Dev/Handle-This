8f11c695c92af376e6a6248e84a715e2c9da549b
Christoph551 <christophersimmonds551@gmail.com>
2025-04-23 19:12:43 -0700

    Switch to PostgreSQL for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,28 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+    // For Heroku 
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, 
+            },
+        },
+    });
 } else {
+    // Local development 
     sequelize = new Sequelize(
         process.env.DBNAME,
         process.env.DBUSER,
         process.env.DBPASS,
         {
             host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
+            dialect: 'postgres',
+            port: 5432,
         }
     );
 }
